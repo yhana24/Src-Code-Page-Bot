9d1464d453cf3e0359e231417e6a1f8709e17871
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-10-15 20:16:23 +0800

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -7,7 +7,7 @@ module.exports = {
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const query = args.join(' ');
     try {
-      const apiUrl = `https://deku-rest-api-3ijr.onrender.com/search/lyrics?q=${encodeURIComponent(query)}`;
+      const apiUrl = `https://deku-rest-apis.ooguy.com/search/lyrics?q=${encodeURIComponent(query)}`;
       const response = await axios.get(apiUrl);
       const result = response.data.result;
 
